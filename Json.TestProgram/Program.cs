c6e2e3ba2e107f274904efb3f92ca70edc0ccef6
jblego9 <112616246+jblego9@users.noreply.github.com>
2026-07-15 14:57:12 +1000

    Organise into methods

--- Json.TestProgram/Program.cs
+++ Json.TestProgram/Program.cs
@@ -4,13 +4,35 @@ namespace Json.TestProgram;
 
 public class Program
 {
+    private const string dataFilepath = "data.json";
+
     public static void Main()
     {
-        const string dataFilepath = "data.json";
+        if (File.Exists(dataFilepath))
+            HandlePreviousText();
+        else
+            HandleNoPreviousText();
+    }
 
-        if (!File.Exists(dataFilepath))
-        {
-            Console.Write("No previous text.\nEnter text: ");
+    private static string CreateJsonObjectString(string previousText, long modificationCount)
+    {
+        return JsonDocument.Write(
+            new JsonValue.JsonObject([
+                new(
+                    new JsonValue.JsonString("modificationCount"),
+                    new JsonValue.JsonNumber(modificationCount.ToString())
+                ),
+                new(
+                    new JsonValue.JsonString("previousText"),
+                    new JsonValue.JsonString(previousText)
+                )
+            ])
+        );
+    }
+
+    private static void HandleNoPreviousText()
+    {
+        Console.Write("No previous text.\nEnter text: ");
             
             string? input = Console.ReadLine();
             if (input is null)
@@ -19,56 +41,31 @@ public class Program
                 return;
             }
 
-            string jsonObjectString = JsonDocument.Write(
-                new JsonValue.JsonObject([
-                    new(
-                        new JsonValue.JsonString("modificationCount"),
-                        new JsonValue.JsonNumber("1")
-                    ),
-                    new(
-                        new JsonValue.JsonString("previousText"),
-                        new JsonValue.JsonString(input)
-                    )
-                ])
-            );
-
-            using StreamWriter sw = File.CreateText(dataFilepath);
-            {
-                sw.Write(jsonObjectString);
-            }
-            return;
-        } else
-        {
-            var jsonObjectValue = (JsonValue.JsonObject)JsonDocument.Parse(File.ReadAllText(dataFilepath));
-
-            var previousText = ((JsonValue.JsonString)jsonObjectValue.Get("previousText")).Value;
-            var modificationCount = ((JsonValue.JsonNumber)jsonObjectValue.Get("modificationCount")).AsInt64();
-
-            Console.Write($"Previous text: {previousText}\nModification count: {modificationCount}\nEnter text: ");
-
-            string? input = Console.ReadLine();
-            if (input is null)
-            {
-                Console.WriteLine("No text given, quiting...");
-                return;
-            }
-
-            string jsonObjectString = JsonDocument.Write(
-                new JsonValue.JsonObject([
-                    new(
-                        new JsonValue.JsonString("modificationCount"),
-                        new JsonValue.JsonNumber((++modificationCount).ToString())
-                    ),
-                    new(
-                        new JsonValue.JsonString("previousText"),
-                        new JsonValue.JsonString(input)
-                    )
-                ])
-            );
+            string jsonObjectString = CreateJsonObjectString(input, 1);
 
             using StreamWriter sw = File.CreateText(dataFilepath);
             sw.Write(jsonObjectString);
+    }
+
+    private static void HandlePreviousText()
+    {
+        var jsonObjectValue = (JsonValue.JsonObject)JsonDocument.Parse(File.ReadAllText(dataFilepath));
+
+        var previousText = ((JsonValue.JsonString)jsonObjectValue.Get("previousText")).Value;
+        var modificationCount = ((JsonValue.JsonNumber)jsonObjectValue.Get("modificationCount")).AsInt64();
+
+        Console.Write($"Previous text: {previousText}\nModification count: {modificationCount}\nEnter text: ");
+
+        string? input = Console.ReadLine();
+        if (input is null)
+        {
+            Console.WriteLine("No text given, quiting...");
             return;
         }
+
+        string jsonObjectString = CreateJsonObjectString(input, ++modificationCount);
+
+        using StreamWriter sw = File.CreateText(dataFilepath);
+        sw.Write(jsonObjectString);
     }
 }
