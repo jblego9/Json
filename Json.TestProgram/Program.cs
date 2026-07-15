4995d47b49c31e503e90401493c9418e281c4886
jblego9 <112616246+jblego9@users.noreply.github.com>
2026-07-15 14:48:24 +1000

    Created a console test project, discovered issues with string escape sequences

---
+++ Json.TestProgram/Program.cs
@@ -0,0 +1,74 @@
+﻿using Json.Document;
+
+namespace Json.TestProgram;
+
+public class Program
+{
+    public static void Main()
+    {
+        const string dataFilepath = "data.json";
+
+        if (!File.Exists(dataFilepath))
+        {
+            Console.Write("No previous text.\nEnter text: ");
+            
+            string? input = Console.ReadLine();
+            if (input is null)
+            {
+                Console.WriteLine("No text given, quiting...");
+                return;
+            }
+
+            string jsonObjectString = JsonDocument.Write(
+                new JsonValue.JsonObject([
+                    new(
+                        new JsonValue.JsonString("modificationCount"),
+                        new JsonValue.JsonNumber("1")
+                    ),
+                    new(
+                        new JsonValue.JsonString("previousText"),
+                        new JsonValue.JsonString(input)
+                    )
+                ])
+            );
+
+            using StreamWriter sw = File.CreateText(dataFilepath);
+            {
+                sw.Write(jsonObjectString);
+            }
+            return;
+        } else
+        {
+            var jsonObjectValue = (JsonValue.JsonObject)JsonDocument.Parse(File.ReadAllText(dataFilepath));
+
+            var previousText = ((JsonValue.JsonString)jsonObjectValue.Get("previousText")).Value;
+            var modificationCount = ((JsonValue.JsonNumber)jsonObjectValue.Get("modificationCount")).AsInt64();
+
+            Console.Write($"Previous text: {previousText}\nModification count: {modificationCount}\nEnter text: ");
+
+            string? input = Console.ReadLine();
+            if (input is null)
+            {
+                Console.WriteLine("No text given, quiting...");
+                return;
+            }
+
+            string jsonObjectString = JsonDocument.Write(
+                new JsonValue.JsonObject([
+                    new(
+                        new JsonValue.JsonString("modificationCount"),
+                        new JsonValue.JsonNumber((++modificationCount).ToString())
+                    ),
+                    new(
+                        new JsonValue.JsonString("previousText"),
+                        new JsonValue.JsonString(input)
+                    )
+                ])
+            );
+
+            using StreamWriter sw = File.CreateText(dataFilepath);
+            sw.Write(jsonObjectString);
+            return;
+        }
+    }
+}
