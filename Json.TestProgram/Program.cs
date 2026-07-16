86701b87e4bd8d669c56452625d831758d50e219
jblego9 <112616246+jblego9@users.noreply.github.com>
2026-07-16 13:00:03 +1000

    Use JsonSerializer to write new json string

--- Json.TestProgram/Program.cs
+++ Json.TestProgram/Program.cs
@@ -1,67 +1,58 @@
 ﻿using Json.Document;
+using Json.Serialization;
 
 namespace Json.TestProgram;
 
 public class Program
 {
-    private const string dataFilepath = "Json.TestProgram.data.json";
+    private const string DATA_FILEPATH = "Json.TestProgram.data.json";
 
     public static void Main()
     {
-        if (File.Exists(dataFilepath))
+        if (File.Exists(DATA_FILEPATH))
             HandlePreviousText();
         else
             HandleNoPreviousText();
     }
 
     /// <summary>
-    /// Throws <see cref="FormatException"/> on failure.
+    /// Throws <see cref="ArgumentException"/> on serialize failure.
+    /// <para>Throws <see cref="FormatException"/> on write failure.</para>
     /// </summary>
     private static string CreateJsonObjectString(string previousText, long modificationCount)
     {
-        return JsonDocument.Write(
-            new JsonValue.JsonObject([
-                new(
-                    new JsonValue.JsonString("modificationCount"),
-                    new JsonValue.JsonNumber(modificationCount.ToString())
-                ),
-                new(
-                    new JsonValue.JsonString("previousText"),
-                    new JsonValue.JsonString(previousText)
-                )
-            ])
-        );
+        return JsonSerializer.Serialize(new { ModificationCount = modificationCount, PreviousText = previousText });
     }
 
     private static void HandleNoPreviousText()
     {
         Console.Write("No previous text.\nEnter text: ");
             
-            string? input = Console.ReadLine();
-            if (input is null)
-            {
-                Console.WriteLine("No text given, quiting...");
-                return;
-            }
+        string? input = Console.ReadLine();
+        if (input is null)
+        {
+            Console.WriteLine("No text given, quiting...");
+            return;
+        }
 
-            string jsonObjectString;
-            try {
-                jsonObjectString = CreateJsonObjectString(input, 1);
-            } catch (FormatException err) {
-                Console.WriteLine($"{err.Message}, quitting...");
-                return;
-            }
+        string jsonObjectString;
+        try {
+            jsonObjectString = CreateJsonObjectString(input, 1);
+        } catch (FormatException err) {
+            Console.WriteLine($"{err.Message}, quitting...");
+            return;
+        }
 
-            using StreamWriter sw = File.CreateText(dataFilepath);
-            sw.Write(jsonObjectString);
+        using StreamWriter sw = File.CreateText(DATA_FILEPATH);
+        sw.Write(jsonObjectString);
     }
 
     private static void HandlePreviousText()
     {
-        var jsonObjectValue = (JsonValue.JsonObject)JsonDocument.Parse(File.ReadAllText(dataFilepath));
+        var jsonObjectValue = (JsonValue.JsonObject)JsonDocument.Parse(File.ReadAllText(DATA_FILEPATH));
 
-        var previousText = ((JsonValue.JsonString)jsonObjectValue.Get("previousText")).Value;
-        var modificationCount = ((JsonValue.JsonNumber)jsonObjectValue.Get("modificationCount")).AsInt64();
+        var previousText = ((JsonValue.JsonString)jsonObjectValue.Get("PreviousText")).Value;
+        var modificationCount = ((JsonValue.JsonNumber)jsonObjectValue.Get("ModificationCount")).AsInt64();
 
         Console.Write($"Previous text: {previousText}\nModification count: {modificationCount}\nEnter text: ");
 
@@ -72,15 +63,15 @@ public class Program
             return;
         }
 
-            string jsonObjectString;
-            try {
-                jsonObjectString = CreateJsonObjectString(input, ++modificationCount);
-            } catch (FormatException err) {
-                Console.WriteLine($"{err.Message}, quitting...");
-                return;
-            }
+        string jsonObjectString;
+        try {
+            jsonObjectString = CreateJsonObjectString(input, ++modificationCount);
+        } catch (FormatException err) {
+            Console.WriteLine($"{err.Message}, quitting...");
+            return;
+        }
 
-        using StreamWriter sw = File.CreateText(dataFilepath);
+        using StreamWriter sw = File.CreateText(DATA_FILEPATH);
         sw.Write(jsonObjectString);
     }
 }
